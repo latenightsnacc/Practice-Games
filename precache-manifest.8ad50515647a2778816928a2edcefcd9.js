85a1d62bbbd0bacc6e3763a4b0b3656498118cd9
Deborah <54311514+latenightsnacc@users.noreply.github.com>
2020-06-09 17:09:39 -0400

    Updates

---
+++ precache-manifest.8ad50515647a2778816928a2edcefcd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fab932058b2d95e367caa13a8441aa86",
+    "url": "/Practice-Games/index.html"
+  },
+  {
+    "revision": "cc3914ab63a45d111532",
+    "url": "/Practice-Games/static/css/main.462e7312.chunk.css"
+  },
+  {
+    "revision": "c8cc534a8e7fb6a3d40a",
+    "url": "/Practice-Games/static/js/2.e5e3a8f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Practice-Games/static/js/2.e5e3a8f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc3914ab63a45d111532",
+    "url": "/Practice-Games/static/js/main.9e0b228a.chunk.js"
+  },
+  {
+    "revision": "f962415ea630960fc185",
+    "url": "/Practice-Games/static/js/runtime-main.ffda13de.js"
+  }
+]);
